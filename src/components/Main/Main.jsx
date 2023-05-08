66c3e9d9145d08efcf5acb3eb64b9e8b6f6f4445
Денис Исламгалеев <93082620+denisislamgaleevv@users.noreply.github.com>
2023-05-08 20:47:38 +0500

    try

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,8 @@ import './Main.css';
 export function Main() {
   return (
     <div className="Main">
-       <h2>Добро пожаловать на наше веб-приложение с тестами на любой вкус!</h2>
+       <h2>Добро пожаловать на наше веб-приложение<br/>с тестами на любой вкус!</h2>
+       <img src ={'https://12bb6ecf-bda5-4c99-816b-12bda79f6bd9.selcdn.net/upload//Photo_Tovar/17961203_1623865328.jpg'} width='400px'/>
     </div>
   );
 }
